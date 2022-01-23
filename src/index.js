b3a2a6850c5e058dc6fe375c49161b0a53379ced
maxomeleneckii <max.omeleneckii@gmail.com>
2022-01-23 23:12:51 +0300

    feat:add an algorithm for solving the problem

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  let brackets = str.split("");
-  for (let i = 0; i < brackets.length; i++) {
-    for (let x = 0; x < bracketsConfig.length; x++) {
-      if (brackets[i] === bracketsConfig[x][0] && brackets[i + 1] === bracketsConfig[x][1]) {
-        brackets.splice(i, 2);
+  let res = str.split('');
+  for (let i = 0; i < res.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (res[i] === bracketsConfig[j][0] && res[i + 1] === bracketsConfig[j][1]) {
+        res.splice([i], 2);
         i = -1;
         continue;
       }
     }
   }
-  if (brackets.length === 0) {
+  if (res.length === 0) {
     return true;
   } else {
     return false;
